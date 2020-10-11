3661ee4f12be0189ef387e4e714288955c158aa1
Phuong Nguyen <phuongnguyen.th0905@gmail.com>
2020-10-11 09:54:56 +0700

    initialization hybrid

---
+++ testcases/facebook/web/users/Login.java
@@ -0,0 +1,26 @@
+package facebook.web.users;
+
+import org.testng.annotations.Test;
+
+public class Login {
+	@Test
+	public void Login_01_InvalidEmail() {
+		
+	}
+	@Test
+	public void Login_02_IncorrectEmail() {
+		
+	}
+	@Test
+	public void Login_03_InvalidPass() {
+		
+	}
+	@Test
+	public void Login_04_IncorrectPass() {
+		
+	}
+	@Test
+	public void Login_05_InvalidEmailAndPass() {
+
+	}
+}
